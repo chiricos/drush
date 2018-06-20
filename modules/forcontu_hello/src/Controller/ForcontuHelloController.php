1872ee066f1a64af1e5f63c9863ff2e277deec95
Bits <bits@MacBook-Pro-de-Bits-3.local>
2018-06-20 11:10:23 -0500

    creaciòn de modulo con hook

---
+++ modules/forcontu_hello/src/Controller/ForcontuHelloController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+* @file
+* Contains \Drupal\forcontu_hello\Controller\ForcontuHelloController. */
+namespace Drupal\forcontu_hello\Controller; 
+use Drupal\Core\Controller\ControllerBase;
+/**
+* Controlador para devolver el contenido de las páginas definidas */
+class ForcontuHelloController extends ControllerBase {
+	public function hello() 
+	{ 
+		return array(
+		'#markup' => '<p>' . $this->t('Hello, Forcontu! This is my first module in Drupal 8!') . '</p>',
+		); 
+	}
+}
